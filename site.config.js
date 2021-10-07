c7bf69a1957ebdb00a4cbd1a9430077874d3577b
JUCGA <92082946+JUCGA@users.noreply.github.com>
2021-10-07 16:51:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'cdf57796115a4fd5a48d1c036156cb94',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jucga',
+  domain: 'https://www.notion.so/jucga/My-Web-cdf57796115a4fd5a48d1c036156cb94',
+  author: 'Jucga',
 
   // open graph metadata (optional)
   description: 'Example site description',
